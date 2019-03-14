e09ec032246a6e958a178be38731ddc7b643dba6
Denky <sorek230@gmail.com>
2019-03-14 03:51:57 +0100

    Added WTT(word to type), user input handling and updating score.

---
+++ app/src/main/java/com/stucom/grupo4/typhone/activities/PlayActivity.java
@@ -0,0 +1,146 @@
+package com.stucom.grupo4.typhone.activities;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.InputType;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.stucom.grupo4.typhone.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    /* TEST - Remove this when pulling from word pool */
+    String[] wordPool = new String[]{
+            "helicopterus", "holabondia", "whatisthis", "sunormal"
+    };
+    /* ---------------- */
+
+    // Word to type
+    private TextView txtWord;
+    private String[] wordColors;
+    private String lastWord;
+    private int cursor;
+
+    // Score
+    private final int LETTER_RIGHT = 10;
+    private TextView txtScore;
+    private int score;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+        txtWord = findViewById(R.id.txtWord);
+        txtWord.setHorizontallyScrolling(true);
+        txtScore = findViewById(R.id.txtScore);
+
+        startGame();
+    }
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Get user input letter
+        String key = KeyEvent.keyCodeToString(keyCode);
+        char letterTyped = key.substring(key.length() - 1).charAt(0);
+
+        // Validate input letter
+        boolean right = isInputRight(letterTyped);
+        // Take actions based on result
+        if (right) rightInput();
+        else wrongInput();
+        // Update letter color based on result
+        updateLetterStyle(right);
+
+        cursor++;
+        // Word completed = go next word
+        if (cursor == txtWord.length())
+            updateWordToType();
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void startGame() {
+        // Reset game variables
+        lastWord = "";
+        setScore(0);
+        // Get first word
+        updateWordToType();
+    }
+
+    private boolean isInputRight(char letterInput) {
+        String letterToType = String.valueOf(txtWord.getText().charAt(cursor));
+        String letterTyped = String.valueOf(letterInput);
+        return letterTyped.equalsIgnoreCase(letterToType);
+    }
+    private void rightInput() {
+        updateScore(LETTER_RIGHT);
+    }
+    private void wrongInput() {
+
+    }
+
+    /**
+     * Pull non-repeated random word from wordPool.
+     * @return random non-repeated word from wordPool
+     */
+    private String pullWordFromWordPool() {
+        // Keep pulling til non-repeated word comes out
+        String randWord;
+        do {
+            int randIndex = (int) (Math.random() * wordPool.length);
+            randWord = wordPool[randIndex];
+        } while (randWord.equals(lastWord));
+        
+        // Save word pulled so the process can be repeated
+        lastWord = randWord;
+        return randWord;
+    }
+
+    private void updateWordToType() {
+        String newWord = pullWordFromWordPool();
+        txtWord.setText(newWord);
+        initWordStyle(newWord);
+        updateWordStyle();
+        cursor = 0;
+    }
+    private void initWordStyle(String word) {
+        // Init style array with capacity = word.length
+        wordColors = new String[word.length()];
+        // Fill style array with color for each letter
+        for (int i = 0; i < wordColors.length; i++) {
+            wordColors[i] = "black";
+        }
+    }
+    private void updateLetterStyle(boolean isInputRight) {
+        // Determine current letter color
+        String color = isInputRight ? "green" : "red";
+        // Update current letter style
+        wordColors[cursor] = color;
+        // Re-draw word with updated style
+        updateWordStyle();
+    }
+    private void updateWordStyle() {
+        // Build styled word string
+        StringBuilder style = new StringBuilder();
+        char[] letters = txtWord.getText().toString().toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            style.append("<font color='").append(wordColors[i]).append("'>")
+                                        .append(letters[i]).append("</font>");
+        }
+        // Set textView to styled word string
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            txtWord.setText(Html.fromHtml(style.toString(),  Html.FROM_HTML_MODE_LEGACY), TextView.BufferType.SPANNABLE);
+        else txtWord.setText(Html.fromHtml(style.toString()), TextView.BufferType.SPANNABLE);
+    }
+
+    private void updateScore(int scoreDiff) {
+        setScore(this.score + scoreDiff);
+    }
+    private void setScore(int score) {
+        // Update score + textView
+        this.score = score;
+        txtScore.setText(String.valueOf(score));
+    }
+}
---
+++ app/src/main/java/com/stucom/grupo4/typhone/constants/Style.java
@@ -0,0 +1,12 @@
+package com.stucom.grupo4.typhone.constants;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class Style {
+
+    public static final Color right = Color.valueOf(Color.GREEN);
+    public static final Color wrong = Color.valueOf(Color.RED);
+}
