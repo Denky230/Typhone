c0e90196049330adb7f4407337bf95709e07e3db
Daniolo <dani_tarrassotoscano@hotmail.com>
2019-03-22 18:01:20 +0100

    Login, Register and Score layouts

---
+++ app/src/main/java/com/stucom/grupo4/typhone/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.stucom.grupo4.typhone.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stucom.grupo4.typhone.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
