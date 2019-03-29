c3e91de0099b10638327885eada57f575f0d96fb
Daniolo <dani_tarrassotoscano@hotmail.com>
2019-03-29 17:59:14 +0100

    Login, Register and Score layouts

--- app/src/main/java/com/stucom/grupo4/typhone/activities/ScoreboardActivity.java
+++ app/src/main/java/com/stucom/grupo4/typhone/activities/ScoreboardActivity.java
@@ -2,14 +2,24 @@ package com.stucom.grupo4.typhone.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.stucom.grupo4.typhone.R;
 
 public class ScoreboardActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
+        recyclerView = findViewById(R.id.recyclerView);
+
+    }
+
+
+    protected void onPause(){
+        super.onPause();
     }
 }
