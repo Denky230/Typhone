68df4f6051c9262fb7975501403c5cf6476b14f6
Denky <sorek230@gmail.com>
2019-05-22 23:27:55 +0200

    App is now fullscreen (TODO: hide navigation bar)

--- app/src/main/java/com/stucom/grupo4/typhone/activities/HomeActivity.java
+++ app/src/main/java/com/stucom/grupo4/typhone/activities/HomeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.stucom.grupo4.typhone.R;
 
@@ -12,6 +14,12 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // No title, no nav bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_home);
 
         // Play button
@@ -28,7 +36,7 @@ public class HomeActivity extends AppCompatActivity {
         findViewById(R.id.btnScore).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Send to PlayActivity (start new game)
+                // Send to ScoreboardActivity
                 Intent intent = new Intent(HomeActivity.this, ScoreboardActivity.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/stucom/grupo4/typhone/activities/PlayActivity.java
+++ app/src/main/java/com/stucom/grupo4/typhone/activities/PlayActivity.java
@@ -6,6 +6,8 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.stucom.grupo4.typhone.R;
@@ -74,23 +76,22 @@ public class PlayActivity extends AppCompatActivity
         txtScore = findViewById(R.id.lblScore);
         txtGameTimer = findViewById(R.id.lblGameTimer);
 
-        final View decorView = getWindow().getDecorView();
-        final int uiOptions =
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOptions);
-
-        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                Tools.log(String.valueOf(visibility));
-                decorView.setSystemUiVisibility(uiOptions);
-            }
-        });
+//        final View decorView = getWindow().getDecorView();
+//        final int uiOptions =
+//                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+//                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+//                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+//                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+//                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+//        decorView.setSystemUiVisibility(uiOptions);
+//
+//        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+//            @Override
+//            public void onSystemUiVisibilityChange(int visibility) {
+//                Tools.log(String.valueOf(visibility));
+//                decorView.setSystemUiVisibility(uiOptions);
+//            }
+//        });
 
 
         startGame();
